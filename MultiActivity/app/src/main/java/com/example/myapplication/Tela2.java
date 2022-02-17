ddafdc0924d95de6c397189d18b859ddfd93556d
James Rovel Barbosa <diagjames@gmail.com>
2022-02-17 20:59:16 -0300

    activity working

--- MultiActivity/app/src/main/java/com/example/myapplication/Tela2.java
+++ MultiActivity/app/src/main/java/com/example/myapplication/Tela2.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Tela2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tela2);
     }
 
     public void onClick(View view) {
-        Intent it = new Intent(this, Tela2.class);
+        Intent it = new Intent(this, MainActivity.class);
         startActivity(it);
     }
+
 }
